7c330e4f3757753bd6693d7b1e4e6f68f0777b03
jsr25 <ramosjuans@live.com.ar>
2021-08-31 23:36:52 -0500

    creation UserDocument

---
+++ src/main/java/org/ada/school/model/UserDocument.java
@@ -0,0 +1,26 @@
+package org.ada.school.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document
+public class UserDocument {
+    @Id
+    String id;
+
+    String name;
+
+    @Indexed( unique = true )
+    String email;
+
+    String lastName;
+
+    Date createdAt;
+
+    public UserDocument()
+    {
+    }
+}
